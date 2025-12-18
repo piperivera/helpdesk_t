89a681302a368841467721eafa6427fe3af5332f
Felipe Cárdenas <cardenasdaniel178@gmail.com>
2025-12-18 14:14:05 -0500

    deploy 10

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: ["error", "warn"],
+    datasources: {
+      db: {
+        // importante: usar explícitamente DATABASE_URL
+        url: process.env.DATABASE_URL,
+      },
+    },
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// Evita múltiples instancias en dev (Next + HMR)
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
